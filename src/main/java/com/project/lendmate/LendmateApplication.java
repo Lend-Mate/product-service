61448bed4bf5b2e61894bb55a9a61c4c759f7ccb
“timurturbil” <timurturbil@gmail.com>
2026-04-22 22:47:54 +0300

    first commit

---
+++ src/main/java/com/project/lendmate/LendmateApplication.java
@@ -0,0 +1,13 @@
+package com.project.lendmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/lendmate/LendmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.lendmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

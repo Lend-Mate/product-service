20604db55ad0ac6a9149cf62207ea12698a4af29
“timurturbil” <timurturbil@gmail.com>
2026-05-10 17:31:33 +0300

    add h2 database for testing purpose

--- src/test/java/com/project/lendmate/LendmateApplicationTests.java
+++ src/test/java/com/project/lendmate/LendmateApplicationTests.java
@@ -2,8 +2,10 @@ package com.project.lendmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LendmateApplicationTests {
 
 	@Test

9d01569880efb244cca784a2b7db0a7ae8c37ad1
Elif Çelik <elifcelik.eva@gmail.com>
2026-05-14 00:42:46 +0300

    remove blank

--- src/main/java/com/project/lendmate/LendmateApplication.java
+++ src/main/java/com/project/lendmate/LendmateApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LendmateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LendmateApplication.class, args);
-
-
 	}
 }

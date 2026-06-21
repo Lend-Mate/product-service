1d5736e1f7b14a1ab0712b9ddd23ef0eef43edc3
timurturbil <timurturbil@gmail.com>
2026-06-21 14:49:37 +0300

    add cors config only in dev

--- src/main/java/com/project/lendmate/config/CorsConfig.java
+++ src/main/java/com/project/lendmate/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.project.lendmate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev")
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
